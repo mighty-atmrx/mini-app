39997ea347c734ef35205e9ec74d89dca8acac84
Artyom Nik <atmrx@mail.ru>
2025-05-03 12:56:08 +0300

    middleware registration in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\verifyJWT;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(VerifyJWT::class);
+
+        $middleware->group('api', [
+
+        ]);
+
+        $middleware->alias([
+            'verifyJWT' => VerifyJWT::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
