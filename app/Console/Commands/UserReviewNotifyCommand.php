7d1fcb58ab3ac2801e32c768bc5e30735351c766
Artyom Nik <atmrx@mail.ru>
2025-05-27 18:40:22 +0300

    feat(endpoints): added info about endpoints

--- app/Console/Commands/UserReviewNotifyCommand.php
+++ app/Console/Commands/UserReviewNotifyCommand.php
@@ -3,28 +3,15 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class UserReviewNotifyCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'app:user-review-notify-command';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        //
+
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use App\Console\Commands\BookingNotifyCommand;
 use App\Console\Commands\DeleteOldExpertSchedules;
 use App\Console\Commands\UpdateBookingStatus;
+use App\Console\Commands\UserReviewNotifyCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,6 +15,7 @@ class Kernel extends ConsoleKernel
         DeleteOldExpertSchedules::class,
         UpdateBookingStatus::class,
         BookingNotifyCommand::class,
+        UserReviewNotifyCommand::class,
     ];
 
     protected function schedule(Schedule $schedule)
@@ -21,6 +23,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:delete-old-expert-schedules')->dailyAt('00:10');
         $schedule->command('app:update-booking-status')->everyFiveMinutes();
         $schedule->command('app:booking-notify-command')->everyMinute();
+        $schedule->command('app:user-review-notify-command')->everyFiveMinutes();
     }
 
     protected function commands()
