3499dc286306a69dbf00ad9b39b3d2faec455f5f
Artyom Nik <atmrx@mail.ru>
2025-06-17 23:56:40 +0300

    feat(tests): added admin export experts test

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -72,4 +72,38 @@ class AdminTest extends TestCase
         $deleteResponse->assertJsonStructure(['message']);
         $this->assertDatabaseMissing('users', ['id' => $userId]);
     }
+
+    public function test_export_experts(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+        $adminToken = JWTauth::fromUser($admin);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $adminToken)
+            ->get('/api/experts-to-excel');
+
+        $response->assertOk();
+        $response->assertHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        $response->assertHeader('Content-Disposition', 'attachment; filename=experts.xlsx');
+    }
+
+    public function test_non_admin_cannot_export_experts(): void
+    {
+        $user = User::factory()->create(['role' => 'user']);
+        $token = JWTauth::fromUser($user);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->get('/api/experts-to-excel');
+
+        $response->assertForbidden();
+        $response->assertJson([
+            'message' => 'Доступ запрещен.'
+        ]);
+    }
+
+    public function test_unauthenticated_user_cannot_export_experts(): void
+    {
+        $response = $this->get('/api/experts-to-excel');
+
+        $response->assertUnauthorized();
+    }
 }
