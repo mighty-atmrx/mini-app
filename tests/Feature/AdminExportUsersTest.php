a2dc00bcfd489a4a6fbaa7222e701f342286f672
Artyom Nik <atmrx@mail.ru>
2025-06-18 00:02:17 +0300

    feat(tests): added admin export users test

--- tests/Feature/AdminExportUsersTest.php
+++ tests/Feature/AdminExportUsersTest.php
@@ -2,19 +2,46 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AdminExportUsersTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_export_users(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+        $adminToken = JWTauth::fromUser($admin);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $adminToken)
+            ->get('/api/users-to-excel');
+
+        $response->assertOk();
+        $response->assertHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        $response->assertHeader('Content-Disposition', 'attachment; filename=users.xlsx');
+    }
+
+    public function test_non_admin_cannot_export_users(): void
+    {
+        $user = User::factory()->create(['role' => 'user']);
+        $token = JWTauth::fromUser($user);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->get('/api/users-to-excel');
+
+        $response->assertForbidden();
+        $response->assertJson([
+            'message' => 'Доступ запрещен.'
+        ]);
+    }
+
+    public function test_unauthenticated_user_cannot_export_users(): void
+    {
+        $response = $this->get('/api/users-to-excel');
+
+        $response->assertUnauthorized();
     }
 }
