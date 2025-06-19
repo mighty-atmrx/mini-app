77079457f3973bff977ca537dafdae4b9cb615c3
Artyom Nik <atmrx@mail.ru>
2025-06-19 19:43:57 +0300

    feat(tests): added user show test

--- tests/Feature/User/UserShowTest.php
+++ tests/Feature/User/UserShowTest.php
@@ -2,19 +2,47 @@
 
 namespace Tests\Feature\User;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class UserShowTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_user_show(): void
+    {
+        $user = User::factory()->create();
+
+        $token = JWTauth::fromUser($user);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $token)
+            ->getJson("/api/profile/{$user->id}");
+
+        $response->assertOk();
+    }
+
+    public function test_user_cannot_show_other_users(): void
+    {
+        $user = User::factory()->create();
+
+        $otherUser = User::factory()->create();
+        $otherUserToken = JWTAuth::fromUser($otherUser);
+
+        $response = $this->withHeader('Authorization', 'Bearer ' . $otherUserToken)
+            ->getJson("/api/profile/{$user->id}");
+
+        $response->assertStatus(403);
+        $response->assertJsonStructure(['error']);
+    }
+
+    public function test_unauthorized_user_cannot_show_profile(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->getJson('/api/profile/' . $user->id);
+
+        $response->assertStatus(401);
     }
 }
